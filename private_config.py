f0e1bcf8b702844855b03dcf7775a59fff337cad
nmbrgts <tylerjnickerson@gmail.com>
2018-05-21 19:13:07 -0700

    move keys to environ

--- private_config.py
+++ private_config.py
@@ -1,6 +1,7 @@
 '''configuration values for api'''
 from argon2 import PasswordHasher
 from peewee import PostgresqlDatabase
+import os
 
 DEBUG = True
 HOST = '0.0.0.0'
@@ -9,11 +10,20 @@ SECRET_KEY = 'SD:FLKDWUdsfk;aifaw:WEFJuqhw*GF%DR$*&%$eFgYrtUjhuu9IuyR5p^TGIYv'
 DEFAULT_RATE = '100/hour'
 HASHER = PasswordHasher()
 DATABASE = PostgresqlDatabase(
-    'course_db',
-    host='course-api.chamxx5zi4uv.us-west-2.rds.amazonaws.com',
-    port='5432',
-    user='tnick',
-    password='password1',
-    sslmode='verify-ca',
-    sslrootcert='./config/amazon-rds-ca-cert.pem'
+    os.eviron['database'],
+    host=os.environ['host'],
+    port=os.environ['port'],
+    user=os.environ['user'],
+    password=os.environ['password'],
+    sslmode=os.environ['sslmode'],
+    sslrootcert=os.environ['sslrootcert']
 )
+# DATABASE = PostgresqlDatabase(
+#     'course_db',
+#     host='course-api.chamxx5zi4uv.us-west-2.rds.amazonaws.com',
+#     port='5432',
+#     user='tnick',
+#     password='password1',
+#     sslmode='verify-ca',
+#     sslrootcert='./config/amazon-rds-ca-cert.pem'
+# )
